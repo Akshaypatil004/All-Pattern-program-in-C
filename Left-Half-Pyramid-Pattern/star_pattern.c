b682800e973735a0c6ecc93d10742679c17dc720
Akshaypatil004 <akshaypatil00404@gmail.com>
2024-12-03 16:55:29 +0530

    another method added

--- Left-Half-Pyramid-Pattern/star_pattern.c
+++ Left-Half-Pyramid-Pattern/star_pattern.c
@@ -6,22 +6,55 @@
 * * * * *
 
 */
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int n;
+int main()
+{
+  int n;
 
-    printf("Enter number of rows:   ");
-    scanf("%d",&n);
+  printf("Enter number of rows:   ");
+  scanf("%d", &n);
 
-    for(int i=1;i<=n;i++){
-        for(int k=n-1;k>=i;k--){
-            printf("  ");
-        }
-        for(int j=1;j<=i;j++){
-            printf("* ");
-        }
-        printf("\n");
+  for (int i = 1; i <= n; i++)
+  {
+    for (int k = n - 1; k >= i; k--)
+    {
+      printf("  ");
     }
-    return 0;
+    for (int j = 1; j <= i; j++)
+    {
+      printf("* ");
+    }
+    printf("\n");
+  }
+
+  // Here is the another method the to print the above pattern
+
+  // #include <stdio.h>
+
+  //   int main()
+  //   {
+  //     int n;
+
+  //     printf("Enter number of row:    ");
+  //     scanf("%d", &n);
+
+  //     for (int i = 1; i <= n; i++)
+  //     {
+  //       for (int j = 1; j <= n; j++)
+  //       {
+  //         if (i + j <= n)
+  //         {
+  //           printf("  ");
+  //         }
+  //         else
+  //         {
+  //           printf("* ");
+  //         }
+  //       }
+  //       printf("\n");
+  //     }
+  //     return 0;
+  //   }
+  return 0;
 }
