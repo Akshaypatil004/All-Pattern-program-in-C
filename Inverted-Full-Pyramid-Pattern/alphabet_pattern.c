4163d3bc9bf729f0aeaa5590c4782926fb6d1bc8
Akshaypatil004 <akshaypatil00404@gmail.com>
2024-12-04 22:53:08 +0530

    Inverted Full Pyramid Pattern 

---
+++ Inverted-Full-Pyramid-Pattern/alphabet_pattern.c
@@ -0,0 +1,31 @@
+/*
+
+* * * * * * * * *   |   1 2 3 4 5 6 7 8 9   |   A B C D E F G H I
+  * * * * * * *     |     1 2 3 4 5 6 7     |     A B C D E F G
+    * * * * *       |       1 2 3 4 5       |       A B C D E
+      * * *         |         1 2 3         |         A B C
+        *           |           1           |           A
+*/
+#include<stdio.h>
+
+int main(){
+    int n;
+
+    printf("Enter number of row:    ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        char a = 'A';
+        for(int s=1;s<i;s++){
+            printf("  ");
+        }
+        for(int j=n;j>=i;j--){
+            printf("%2c",a++);
+        }
+        for(int k=n-1;k>=i;k--){
+            printf("%2c",a++);
+        }
+        printf("\n");
+    }
+    return 0;
+}
